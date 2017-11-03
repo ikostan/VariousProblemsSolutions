1b621402500329dd3aef16b24fa3dc021e442d7d
ikostan <superegorius@gmail.com>
2017-11-03 16:59:25 -0700

    Start a new project

---
+++ VariousProblemsSolutions/ReverseIntegerProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariousProblemsSolutions
+{
+    class Program
+    {
+        /// <summary>
+        /// 7. Reverse Integer
+        /// Source: https://leetcode.com/problems/reverse-integer/description/
+        /// Reverse digits of an integer.
+        /// Example1: x = 123, return 321
+        /// Example2: x = -123, return -
+        /// Note: The input is assumed to be a 32-bit signed integer.Your function should return 0 when the reversed integer overflows.
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
