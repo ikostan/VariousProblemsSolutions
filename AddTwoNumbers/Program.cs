3e172bb46826442f86e0d442bf39d58b8e9d532a
ikostan <superegorius@gmail.com>
2017-11-03 20:40:22 -0700

    AddTwoNumbers > DONE

--- AddTwoNumbers/Program.cs
+++ AddTwoNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace AddTwoNumbers
 {
@@ -6,7 +8,8 @@ namespace AddTwoNumbers
     {
 
         /// <summary>
-        /// 2. Add Two Numbers
+        /// 
+        /// 2. Add Two Numbers:
         /// Source: https://leetcode.com/problems/add-two-numbers/description/
         /// You are given two non-empty linked lists representing two non-negative integers. 
         /// The digits are stored in reverse order and each of their nodes contain a single digit. 
@@ -14,35 +17,70 @@ namespace AddTwoNumbers
         /// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
         /// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
         /// Output: 7 -> 0 -> 8
+        /// 
         /// </summary>
         /// <param name="args"></param>
 
 
         static void Main(string[] args)
         {
-
+            // Test cases: 
+            // [2,4,3] and [5,6,4] ==> [7,0,8]
+            // [5] and [5] ==> [0,1]
+            // [9] and [9] ==> [8,1]
+            // [1,8] and [0] ==> [1,8]
 
             Console.ReadKey();
         }
 
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            ListNode resultListNode = null;
+            ListNode a = l1, b = l2, first = new ListNode(0), current = first;
+            List<ListNode> values = new List<ListNode>();
+            int reminder = 0;
 
+            while (a != null || b != null)
+            {
+                int sum = ((a != null) ? a.val : 0) + ((b != null) ? b.val : 0) + reminder;
 
+                current.next = new ListNode(((sum >= 10) ? sum % 10 : sum));
+                current = current.next;
 
-            return resultListNode;
+                if (sum == 10)
+                {
+                    reminder = 1;
+                }
+                else if (sum > 10)
+                {
+                    reminder = sum / 10;
+                }
+                else
+                {
+                    reminder = 0;
+                }
+
+                a = (a != null) ? a.next : null;
+                b = (b != null) ? b.next : null;
+            }
+
+            if (reminder > 0)
+            {
+                current.next = new ListNode(reminder);
+                current = current.next;
+            }
+
+            return first.next;
         }
-    }
 
-    /// <summary>
-    ///  Definition for singly-linked list:
-    /// </summary>
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int x) { val = x; }
-    }
+        /// <summary>
+        ///  Definition for singly-linked list:
+        /// </summary>
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
 
-}
+    }
