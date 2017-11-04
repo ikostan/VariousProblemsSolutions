08ac7779f07781710546cdbb9bcf6f2a3a4c1a45
ikostan <superegorius@gmail.com>
2017-11-03 18:27:22 -0700

    Reverse method: DONE

--- VariousProblemsSolutions/ReverseIntegerProgram.cs
+++ VariousProblemsSolutions/ReverseIntegerProgram.cs
@@ -18,6 +18,15 @@ namespace VariousProblemsSolutions
         static void Main(string[] args)
         {
             //Test cases:
+            //SimpleReverseTester();
+            ReverseTester();
+
+            Console.ReadKey();
+        }
+
+        private static void SimpleReverseTester() {
+
+            //Test cases:
             Console.WriteLine(SimpleReverse(0));
             Console.WriteLine(SimpleReverse(1));
             Console.WriteLine(SimpleReverse(-1));
@@ -27,14 +36,49 @@ namespace VariousProblemsSolutions
             Console.WriteLine(SimpleReverse(2147483647));
             Console.WriteLine(SimpleReverse(Int32.MaxValue));
             Console.WriteLine(SimpleReverse(Int32.MinValue));
-
-            Console.ReadKey();
         }
 
+        private static void ReverseTester()
+        {
 
+            //Test cases:
+            Console.WriteLine(Reverse(0));
+            Console.WriteLine(Reverse(1));
+            Console.WriteLine(Reverse(-1));
+            Console.WriteLine(Reverse(321));
+            Console.WriteLine(Reverse(-321));
+            Console.WriteLine(Reverse(-2147483648));
+            Console.WriteLine(Reverse(2147483647));
+            Console.WriteLine(Reverse(Int32.MaxValue));
+            Console.WriteLine(Reverse(Int32.MinValue));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static int Reverse(int x) {
 
             int result = 0;
+
+            while (x != 0)
+            {
+                int remaind = 0;
+
+                //Quotient: a result obtained by dividing one quantity by another.
+                x = Math.DivRem(x, 10, out remaind);              
+
+                try
+                {
+                    result = checked(result * 10 + remaind);
+                    Debug.WriteLine($"x: {x}, remaind: {remaind}, result: {result}");
+                }
+                catch (OverflowException)
+                {
+                    return 0;
+                }
+            }
 
             return result;
         }
