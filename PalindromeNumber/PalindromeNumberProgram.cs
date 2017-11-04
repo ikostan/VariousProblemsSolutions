4761efbad139be486dcd47048999aa8d096473bb
ikostan <superegorius@gmail.com>
2017-11-03 21:22:56 -0700

    New project: Palindrome Number

---
+++ PalindromeNumber/PalindromeNumberProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class PalindromeNumberProgram
+    {
+        /// <summary>
+        /// 9. Palindrome Number
+        /// A palindromic number or numeral palindrome is a number that remains the same when its digits are reversed. 
+        /// Determine whether an integer is a palindrome. Do this without extra space.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
