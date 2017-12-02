cbd5e10370d8a3937b334d9d46e604a5bbe892c2
ikostan <superegorius@gmail.com>
2017-12-02 01:35:39 -0800

    Longest Common Prefix >>> DONE!!!

--- LongestCommonPrefix/Program.cs
+++ LongestCommonPrefix/Program.cs
@@ -23,19 +23,22 @@ namespace LongestCommonPrefix
 
             //Test cases:
             List<string[]> strList = new List<string[]>();
-            strList.Add(new string[] { "" });               //Returns empty string
-            strList.Add(new string[] { "c", "c" });          //Returns "c"
-            //strList.Add(new string[] { "a", "b" });         //Returns empty string
-            //strList.Add(new string[] { "a", "a", "b" });    //Returns empty string
-            //strList.Add(new string[] { "ca", "a" });        //Returns empty string
-            //strList.Add(new string[] { "ca", "c" });        //Returns empty string
-            //strList.Add(new string[] { "aa", "a" });        //Returns "a"
+            strList.Add(new string[] { "" });                                   //Returns empty string
+            strList.Add(new string[] { "c", "c" });                             //Returns "c"
+            strList.Add(new string[] { "aa", "aa" });                           //Returns "aa"
+            strList.Add(new string[] { "a", "b" });                             //Returns empty string
+            strList.Add(new string[] { "a", "a", "b" });                        //Returns empty string
+            strList.Add(new string[] { "ca", "a" });                            //Returns empty string
+            strList.Add(new string[] { "ca", "c" });                            //Returns "c"
+            strList.Add(new string[] { "aa", "a" });                            //Returns "a"
+            strList.Add(new string[] { "c", "acc", "ccc" });                    //Returns empty string
+            strList.Add(new string[] { "aac", "acab", "aa", "abba", "aa" });    //Returns "a"
 
             //Main method execution:
             foreach (string[] item in strList)
             {
                 Console.WriteLine(prg.LongestCommonPrefix(item));
-            }                
+            }
 
             Console.ReadKey();
         }
@@ -48,6 +51,7 @@ namespace LongestCommonPrefix
         public string LongestCommonPrefix(string[] strs)
         {
             StringBuilder prefix = new StringBuilder("");
+            List<bool> lst = new List<bool>();
 
             if (strs.Length == 0)
             {
@@ -59,17 +63,53 @@ namespace LongestCommonPrefix
             }
             else
             {
-                int min = GetShortestStringIndex(strs);
+                int min = GetShortestStringIndex(strs); //Shortest string id
+                Debug.WriteLine($"min indx: {min}");
+                string pfx = strs[min];
 
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    bool isExist = false;
 
+                    if (i != min)
+                    {
+                        for (int b = pfx.Length; b > 0; b--)
+                        {
+                            string a = strs[i].Substring(0, b);
+                            string c = pfx.Substring(0, b);
+
+                            Debug.WriteLine($"a: {a}, c: {c}, b: {b}");
+
+                            if (a == c)
+                            {
+                                isExist = true;
+
+                                prefix.Remove(0, prefix.Length);
+                                prefix.Insert(0, pfx.Substring(0, b));
+                                pfx = prefix.ToString();
+
+                                break;
+                            }
+                        }
+
+                        lst.Add(isExist);
+                    }
+                }
             }
 
-            return prefix.ToString();
+            if (lst.Contains(false))
+            {
+                return "";
+            }
+            else
+            {
+                return prefix.ToString();
+            }
         }
 
 
         /// <summary>
-        ///Get id of the shortest string
+        /// Get id of the shortest string
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
