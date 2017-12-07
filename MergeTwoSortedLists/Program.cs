954d27eb804e2865c284d903e08e95fbdbdbea17
ikostan <superegorius@gmail.com>
2017-12-06 21:24:35 -0800

    Working on Merge Two Sorted Lists

---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MergeTwoSortedLists
+{
+    class Program
+    {
+        /*
+         21. Merge Two Sorted Lists
+
+            Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+            Example:
+
+            Input: 1->2->4, 1->3->4
+            Output: 1->1->2->3->4->4
+
+          Source: https://leetcode.com/problems/merge-two-sorted-lists/description/   
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
