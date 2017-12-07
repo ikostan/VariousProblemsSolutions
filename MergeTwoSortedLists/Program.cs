5cda6fd34aecdfd90d6057cb7238a1969f17ad3f
ikostan <superegorius@gmail.com>
2017-12-07 01:13:18 -0800

    MergeTwoSortedLists >>> Test case ready

--- MergeTwoSortedLists/Program.cs
+++ MergeTwoSortedLists/Program.cs
@@ -32,7 +32,38 @@ namespace MergeTwoSortedLists
         {
             Console.WriteLine("21. Merge Two Sorted Lists:\n");
 
+            Program prg = new Program();
+
             //Test cases: [1,2,4] [1,3,4]
+
+            int[] first = { 1, 2, 4 };
+            int[] second = { 1, 3, 5 };
+
+            ListNode start1 = new ListNode(-1);
+            ListNode start2 = new ListNode(-1);
+            ListNode l1 = start1;
+            ListNode l2 = start2;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                l1.next = new ListNode(first[i]);
+                l1 = l1.next;
+
+                l2.next = new ListNode(second[i]);
+                l2 = l2.next;
+
+                System.Diagnostics.Debug.WriteLine($"{l1.val} {l2.val}");
+            }
+
+            ListNode a = prg.MergeTwoLists(start1.next, start2.next);
+
+            do
+            {
+                Console.WriteLine(a.val);
+                a = a.next;
+            }
+            while (a.next != null);
+            Console.WriteLine(a.val); //Last one
 
             Console.ReadKey();
         }
@@ -66,11 +97,13 @@ namespace MergeTwoSortedLists
                     }
 
                     list = list.next;
+                    System.Diagnostics.Debug.WriteLine($"Log (list): {list.val}");
                 }
             }
 
             list.next = (l1 == null) ? l2 : l1;
 
+            System.Diagnostics.Debug.WriteLine($"Return (list): {first.next.val}");
             return first.next;
         }
     }
